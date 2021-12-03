6162a18dfdb156e49ed0fbd60b2c9412a7b629ad
jayrunkel <jayrunkel@gmail.com>
2021-12-03 12:03:27 -0500

    implemented sprint 3 (search) for MongoDB API

---
+++ api/mongodb/mongoAPISprint3.js
@@ -0,0 +1,513 @@
+// ****************************************************************
+// Sprint 1
+//
+// Description:
+//
+//Create a microservice for managing technical support tickets. Operations include:
+// - creating a ticket
+// - changing ticket status from “open”, “pending”, and “closed”
+// - Updating ticket description
+//
+// Tickets must have the following fields:
+// - ticket number
+// - title
+// - description
+// - open date
+// - close date
+// - ticket owner (the person who created the ticket)
+//
+// ================================================================
+// Sprint 2 Description:
+// Allow multiple people to work on tickets. Tickets have an owner plus
+// multiple other users who can work on the ticket
+//
+// Implementation Notes:
+//  1. Need to add user table
+//  2. Need to convert description field into a table with time stamped comments
+//
+//
+// ================================================================
+// Sprint 3 Description:
+//
+// Add the ability to search for and retrieve tickets based upon searches
+// for words and phrases in ticket title and comments. Matches in ticket
+// title should be weighted over matches in comments and returned first.
+// Search should include typeahead completion and fuzzy search.
+//
+// Implementation Notes:
+//
+// Add to additional API calls for search
+//  1. getSearchCompletions(phrase): returns an array of strings representing the
+//     completions of the phrase
+//  2. search(phrase, fuzzyDistanct, numTickets): returns an array of tickets objects
+//     (all ticket fields) of max length <numTickets> with:
+//        - tickets matching in title first returned before tickets matching in comments
+//        - includes snippets (highlighting) of ticket text that matched the search
+//        - returns fuzzy matches of 1 character distance
+//
+// ****************************************************************
+
+import { MongoClient } from "mongodb";
+const validTicketStatuses = ["open", "closed", "inProgress"]
+
+const dbName = "techSupport"
+const ticketCollectionName = "ticket"
+const usersCollectionName = "users"
+const currentVersion = 2
+
+
+// Create a new MongoClient
+//const client = new MongoClient(uri);
+var client = null
+
+export async function connectToDatabase (uri) {
+    client = new MongoClient(uri);
+    await client.connect();
+    return client;
+}
+
+export async function disconnectFromDatabase () {
+    await client.close();
+}
+
+// ================================================================
+// USER API
+// ++ createUser
+// ++ getUserId
+// ================================================================
+
+async function getNextUserId() {
+	let db = client.db(dbName)
+	let col = db.collection(usersCollectionName)
+	let lastUserId = 0
+	try {
+		let lastUser = await col.find({}, {userId : 1}).sort({userId : -1}).limit(1).toArray()
+		if (lastUser.length == 1) {
+			lastUserId = lastUser[0].userId
+		}
+	}catch (err) {
+		console.log(err.stack)
+	}
+
+	return ++lastUserId
+}
+
+async function createUniqueUser(first, last, title, tryNumber, error) {
+	if (tryNumber > 5) {
+		throw error
+
+	} else {
+		let db = client.db(dbName)
+		let col = db.collection(usersCollectionName)
+		try {
+			const newUserId = await getNextUserId()
+
+			const res = await col.insertOne({firstName: first, lastName: last, title: title, userId: newUserId})
+			console.log(res)
+			return newUserId
+		} catch (err) {
+			return await createUniqueUser(first, last, title, ++tryNumber, err)
+		}
+	}
+	
+}
+
+export async function createUser (first, last, title) {
+    try {
+        const newUserId = await createUniqueUser(first, last, title, 1, null);
+        return newUserId;
+    } catch (err) {
+        console.log(err.stack);
+    }
+}
+
+
+export  async function getUserId  (first, last) {
+    let db = client.db(dbName);
+    let col = db.collection(usersCollectionName);
+
+    try {
+        const user = await col.findOne({ firstName: first, lastName: last });
+        return user ? user.userId : null;
+    } catch (err) {
+        console.log(err.stack);
+    }
+}
+
+
+
+// ================================================================
+// TICKET API
+// ++ createTicket
+// ++ getTicket
+// ++ changeTicketStatus
+// ++ addTicketComment
+// ++ getTicketComments
+//
+// New in Sprint 3
+// ++ getSearchCompletions
+// ++ search
+// ================================================================
+
+async function getNextTicketNum() {
+	let db = client.db(dbName)
+	let col = db.collection(ticketCollectionName)
+	let lastTicketNum = 0
+	try {
+		let lastTicket = await col.find({}, {ticketNumber : 1}).sort({ticketNumber : -1}).limit(1).toArray()
+		if (lastTicket.length == 1) {
+			lastTicketNum = lastTicket[0].ticketNumber 
+		}
+	}catch (err) {
+		console.log(err.stack)
+	}
+
+	return ++lastTicketNum
+}
+
+// This implementation assumes that there is an unique index on ticketNumber
+async function createUniqueTicket(owner, subject, description, tryNumber, error) {
+	if (tryNumber > 5) {
+		throw error
+
+	} else {
+		let db = client.db(dbName)
+		let col = db.collection(ticketCollectionName)
+		try {
+			const newTicketNum = await getNextTicketNum()
+
+			const res = await col.insertOne({
+				ticketNumber: newTicketNum,
+				ticketOwner: owner,
+				subject: subject,
+				version: currentVersion,
+				status: "open",
+				comments: [{
+					userId : owner,
+					date : new Date(),
+					comment: description
+				}]
+			})
+			console.log(res)
+			return newTicketNum
+		} catch (err) {
+			return await createUniqueTicket(owner, subject, description, ++tryNumber, err)
+		}
+	}
+	
+}
+
+export  async function createTicket (owner, subject, description)  {
+    let db = client.db(dbName);
+    let col = db.collection(ticketCollectionName);
+    try {
+        const newTicketNum = await createUniqueTicket(owner, subject, description, 1, null);
+        return newTicketNum;
+    } catch (err) {
+        console.log(err.stack);
+    }
+}
+
+
+
+		
+
+async function convertTicketToCurrentVersion(ticketNumber, userName, version, additionalStage = null) {
+	let db = client.db(dbName)
+	let col = db.collection(ticketCollectionName)
+
+	if (version === undefined || version == 1) {
+		try {
+			const userId = await getUserId(userName, null)
+			let convertVersion1ToVersion2Pipeline = [
+				{
+					$set: {
+						version: currentVersion,
+						ticketOwner: userId,
+						comments: {
+							$concatArrays : [
+								[{
+									userId: userId,
+									date: '$$NOW',
+									comment: '$description'
+								}],
+								{$ifNull : ["$comments", []]}
+							]
+						}
+					}
+				},
+				{
+					$unset: 'description'
+				}
+			]
+			if (additionalStage) {
+				convertVersion1ToVersion2Pipeline.push(additionalStage)
+			}
+			const res = await col.findOneAndUpdate(
+				{ticketNumber : ticketNumber}, //query
+				convertVersion1ToVersion2Pipeline, // update pipeline
+				{returnDocument : "after"} // return the updated document
+			)
+			return res.value
+		} catch (err) {
+			console.log(err.stack)
+		}
+	}
+	else {
+		return null
+	}
+}
+
+export  async function getTicket (ticketNumber)  {
+    let db = client.db(dbName);
+    let col = db.collection(ticketCollectionName);
+    try {
+        let res = await col.findOne({ ticketNumber: ticketNumber });
+        res = (res == null || res.version == currentVersion) ? res : await convertTicketToCurrentVersion(ticketNumber, res.ticketOwner, res.version);
+        console.log(res);
+        return res;
+    }
+    catch (err) {
+        console.log(err.stack);
+    }
+}
+
+export  async function changeTicketStatus (ticketNumber, newStatus)  {
+    let operationStatus = false;
+    let db = client.db(dbName);
+    let col = db.collection(ticketCollectionName);
+
+    if (validTicketStatuses.includes(newStatus)) {
+        try {
+            let res = (newStatus == "closed") ?
+                await col.findOneAndUpdate({ ticketNumber: ticketNumber },
+                    [{ $set: { status: newStatus, closeDate: "$$NOW" } }],
+                    { returnDocument: "after" }) :
+                await col.findOneAndUpdate({ ticketNumber: ticketNumber },
+                    { $set: { status: newStatus } },
+                    { returnDocument: "after" });
+            if (res.version === undefined || res.version == 1)
+                res = await convertTicketToCurrentVersion(res.ticketNumber, res.ticketOwner, res.version);
+            operationStatus = true;
+        } catch (err) {
+            console.log(err.stack);
+        }
+    } else {
+        console.log("[ERROR] Attempting to change status of ticket: ", ticketNumber, " to and invalid ticket status: ", newStatus);
+    }
+
+    return operationStatus;
+}
+
+export  async function addTicketComment  (ticketNumber, userId, description)  {
+    let operationStatus = false;
+    let db = client.db(dbName);
+    let col = db.collection(ticketCollectionName);
+    try {
+      let res = await col.findOneAndUpdate({ ticketNumber: ticketNumber },
+																					 [{
+																						 $set: {
+																							 comments: {
+																								 $concatArrays: [
+																									 { $ifNull: ["$comments", []] },
+																									 [{
+																										 userId: userId,
+																										 date: "$$NOW",
+																										 comment: description
+																									 }]
+																								 ]
+																							 }
+																						 }
+																					 }],
+																					 { returnDocument: "after" });
+      if (res.value != null) {
+        if (res.value.version === undefined || res.value.version == 1)
+          res = await convertTicketToCurrentVersion(res.value.ticketNumber, res.value.ticketOwner, res.value.version);
+        operationStatus = true;
+      }
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+    return operationStatus;
+}
+
+export async function getTicketComments (ticketNumber)  {
+    let db = client.db(dbName);
+    let col = db.collection(ticketCollectionName);
+    try {
+        let res = await col.findOne({ ticketNumber: ticketNumber }, { _id: 0, ticketNumber: 1 });
+        return res == null ? null : res.comments;
+    }
+    catch (err) {
+        console.log(err.stack);
+    }
+}
+
+export async function getSearchCompletions(phrase, numCompletions) {
+  const autocompletePipeline = [
+		{$search: {
+			index: 'autocomplete',
+			autocomplete: {
+				path: 'subject',
+				query: phrase
+			}
+		}}, {$limit: numCompletions}, {$group: {
+			_id: '$subject'
+		}}]
+
+	let db = client.db(dbName);
+  let col = db.collection(ticketCollectionName);
+	let res = null
+  try {
+    let searchRes = await col.aggregate(autocompletePipeline).toArray()
+		res = searchRes.map(doc => doc._id)
+  }
+  catch (err) {
+    console.log(err.stack);
+  }
+
+	return res
+}
+
+
+
+// description 
+export async function search(phrase, fuzzyDistance, numResults) {
+	const searchPipeline = [
+		{$search: {
+			index: 'default',
+			compound: {
+				should: [
+					{
+						text: {
+							query: phrase,
+							path: [
+								'subject'
+							],
+							score: {
+								boost: {
+									value: 10
+								}
+							},
+							fuzzy: {
+								maxEdits: fuzzyDistance
+							}
+						}
+					},
+					{
+						text: {
+							query: phrase,
+							path: [
+								'comments.comment'
+							],
+							score: {
+								boost: {
+									value: 5
+								}
+							},
+							fuzzy: {
+								maxEdits: fuzzyDistance
+							}
+						}
+					},
+					{
+						text: {
+							query: phrase,
+							path: [
+								'description'
+							],
+							score: {
+								boost: {
+									value: 0.5
+								}
+							},
+							fuzzy: {
+								maxEdits: fuzzyDistance
+							}
+						}
+					}
+				],
+				minimumShouldMatch: 1
+			},
+			highlight: {
+				path: [
+					'subject',
+					'description',
+					'comments.comment'
+				]
+			}
+		}}, {$addFields: {
+			score: {
+				$meta: 'searchScore'
+			},
+			highlights: {
+				$meta: 'searchHighlights'
+			}
+		}}, {$limit : numResults}]
+	
+	let db = client.db(dbName);
+  let col = db.collection(ticketCollectionName);
+	let res = null
+  try {
+    res = await col.aggregate(searchPipeline).toArray()
+  }
+  catch (err) {
+    console.log(err.stack);
+  }
+
+	return res
+}
+
+
+/*
+async function test () {
+	let connection = await connectToDatabase(uri)
+
+	let nextUserId = await getNextUserId()
+	console.log("Next user id: ", nextUserId)
+
+	let newUserTom = await createUser("Tom", "Jones", "Boss")
+	console.log("Bosses user id: ", newUserTom)
+	let newUserTomId = await getUserId("Tom", "Jones")
+	console.log("Validating Boss is in the database. User Id: ", newUserTomId)
+
+	let ticketNum = await createTicket(newUserTomId, "Help?", "Doesn't work")
+	console.log("New Ticket Number: ", ticketNum)
+	console.log("New Ticket", await getTicket(ticketNum))
+	console.log("Another Ticket", await getTicket(7))
+
+	let nonExistingTicket = await getTicket(13902343)
+	console.log("Value of non existing ticket: ", nonExistingTicket)
+
+	await changeTicketStatus(5, "closed")
+	console.log("Closed Ticket:", await getTicket(5))
+
+	await changeTicketStatus(ticketNum, "working on it")
+
+	await addTicketComment(4, newUserTom, "Made a change to the ticket")
+	console.log("Updated Ticket Description:", await getTicket(4))
+
+	await addTicketComment(9934393, newUserTom, "This ticket does not exist")
+
+	console.log("The ticket comments are: ", await getTicketComments(4))
+
+	console.log("The comments for a non-existant ticket are ", await getTicketComments(9934393))
+	
+	await disconnectFromDatabase()
+}
+
+async function miniTest() {
+	console.log(client)
+	await connectToDatabase(uri)
+
+	let ticketNum = await getNextTicketNum()
+	console.log("Create New Ticket: ", ticketNum)
+
+	let userId = await getNextUserId()
+	console.log("Create New User: ", userId)
+	await disconnectFromDatabase()
+}
+
+test()
+*/
---
+++ test/sprint3/sprint3Test.js
@@ -0,0 +1,71 @@
+import * as mongoAPI from '../../api/mongodb/mongoAPISprint3.js'
+import * as postgresAPI from '../../api/sql/sqlAPISprint2.js'
+
+var dbAPI = null
+var dbConnSetup = null
+const mongoDBUri = "mongodb+srv://admin:power_low12@realmcluster.aamtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const postgresConnObj = {
+	host: 'localhost',
+	user: 'jayrunkel',
+	database: 'techSupport'
+}
+
+const args = process.argv.slice(2)
+
+function setDBAPI() {
+	switch (args[0]) {
+	case "MongoDB":
+		dbAPI = mongoAPI
+		dbConnSetup = mongoDBUri
+		break
+	case "SQL" :
+		dbAPI = postgresAPI
+		dbConnSetup = postgresConnObj
+		break
+	default:
+		console.log("ERROR - Unknown database API")
+	}
+}
+
+async function test () {
+	let connection = await dbAPI.connectToDatabase(dbConnSetup)
+
+	let newUserTom = await dbAPI.createUser("Tom", "Jones", "Boss")
+	console.log("Bosses user id: ", newUserTom)
+	let newUserTomId = await dbAPI.getUserId("Tom", "Jones")
+	console.log("Validating Boss is in the database. User Id: ", newUserTomId)
+
+	let ticketNum = await dbAPI.createTicket(newUserTomId, "Help?", "Doesn't work")
+	console.log("New Ticket Number: ", ticketNum)
+	console.log("New Ticket", await dbAPI.getTicket(ticketNum))
+	console.log("Another Ticket", await dbAPI.getTicket(7))
+
+	let nonExistingTicket = await dbAPI.getTicket(13902343)
+	console.log("Value of non existing ticket: ", nonExistingTicket)
+
+	await dbAPI.changeTicketStatus(5, "closed")
+	console.log("Closed Ticket:", await dbAPI.getTicket(5))
+
+	let result = await dbAPI.addTicketComment(4, newUserTom, "Made a change to the ticket")
+	console.log("Updated Ticket Description [Success was ", result, "]:", await dbAPI.getTicket(4))
+
+	result = await dbAPI.addTicketComment(9934393, newUserTom, "This ticket does not exist")
+	console.log("Updated Ticket Description for Non Existing Ticket [Success was ", result, "]")
+
+	console.log("The ticket comments are: ", await dbAPI.getTicketComments(4))
+
+	console.log("The comments for a non-existant ticket are ", await dbAPI.getTicketComments(9934393))
+
+	console.log("The search completions for 'hel' are : ", await dbAPI.getSearchCompletions("hel", 10))
+	console.log("The search completions for 'foo' are : ", await dbAPI.getSearchCompletions("foo", 10))
+
+	console.log("Search results for 'hel' search:")
+	console.log(await dbAPI.search("hel", 1, 5))
+	
+	await dbAPI.disconnectFromDatabase()
+}
+
+	(() => {
+		setDBAPI()
+		if (dbAPI != null) test()
+	})()
